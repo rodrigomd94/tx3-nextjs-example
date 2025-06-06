b1ab54a628af4d7551d8e346d8988db7bad61af6
Rodrigo Molina <rodrigomd94@gmail.com>
2025-06-06 10:51:15 -0600

    chore: remove unnecessary next config

--- next.config.ts
+++ next.config.ts
@@ -2,44 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        '@tx3': './tx3/bindings',
-      },
-    },
-  },
-  webpack: (config) => {
-    // Add alias for @tx3
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@tx3': './tx3/bindings',
-    };
-
-    // Configure webpack to handle TypeScript files in .tx3 directory
-    config.module.rules.push({
-      test: /\.ts$/,
-      include: /node_modules\/\.tx3/,
-      use: [
-        {
-          loader: 'ts-loader',
-          options: {
-            transpileOnly: true,
-            compilerOptions: {
-              module: 'esnext',
-              target: 'es2017',
-              moduleResolution: 'node',
-              esModuleInterop: true,
-              allowSyntheticDefaultImports: true,
-              skipLibCheck: true,
-            },
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
+ 
 };
 
 export default nextConfig;
